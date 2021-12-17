40e03495b004a58e20cd120dc3796a5c59cc14aa
akiftasbas <32tasbasakif32@gmail.com>
2021-12-17 14:45:34 +0100

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HAllo");
+        System.out.println("Hallo");
         System.out.println("Developing HelloFeature");
+        System.out.println("Local Repo Repo is already published");
+        System.out.println("Doing more work");
     }
 }
